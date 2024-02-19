ac8c68db9f96cfff7cfb8c31adbacc5b5c46239c
khaledali0907 <khaledali0907@gmail.com>
2024-02-19 20:13:33 +0200

    task#1-edit

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,37 +4,32 @@
 Python script that exports data in the CSV format
 """
 
-import csv
 from requests import get
 from sys import argv
+import csv
 
+if __name__ == "__main__":
+    response = get("https://jsonplaceholder.typicode.com/todos/")
+    data = response.json()
 
-def main():
-    res = get("https://jsonplaceholder.typicode.com/todos/")
-    res2 = get("https://jsonplaceholder.typicode.com/users")
-    todos = res.json()
-    users = res2.json()
+    row = []
+    response2 = get("https://jsonplaceholder.typicode.com/users")
+    data2 = response2.json()
 
-    userid = int(argv[1])
+    for i in data2:
+        if i["id"] == int(argv[1]):
+            employee = i["username"]
 
-    for user in users:
-        if user['id'] == userid:
-            employee = user['username']
-
-    with open(argv[1] + '.csv', 'w', newline='') as file:
+    with open(argv[1] + ".csv", "w", newline="") as file:
         writ = csv.writer(file, quoting=csv.QUOTE_ALL)
 
         for i in data:
 
             row = []
-            if i['userId'] == int(argv[1]):
-                row.append(i['userId'])
+            if i["userId"] == int(argv[1]):
+                row.append(i["userId"])
                 row.append(employee)
-                row.append(i['completed'])
-                row.append(i['title'])
+                row.append(i["completed"])
+                row.append(i["title"])
 
                 writ.writerow(row)
-
-
-if __name__ == '__main':
-    main()
